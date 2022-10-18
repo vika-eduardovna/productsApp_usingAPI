6120a06ce1b86a9f598dc2a91e1090b55649ec98
Vika <viktoria.wout@gmail.com>
2022-10-18 21:33:51 +0200

    some additions..

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import s from './style.module.sass'
-//import UnvisibleCard from '../UnvisibleCard/index'
-//import { useState } from 'react'
+import UnvisibleCard from '../UnvisibleCard/index'
+import { useState } from 'react'
 import { useContext } from 'react'
 import { Context } from '../../context'
 
 
+export default function ProductItem({ id, image, title, description, price }) {
+  const { removeProd } = useContext(Context);
+  const [style, setStyle] = useState({display: 'none' });
 
-export default function ProductItem({id, image, title, description, price }) {
-  const {removeProd} = useContext(Context);
-
-  //const [hover, setHover] = useState(false);
-  //const isHover = hover ? <UnvisibleCard/> : '';
- 
   return (
-    <div className={s.item} onDoubleClick={() => removeProd(id)}>
+    <div
+      className={s.item}
+      onDoubleClick={() => removeProd(id)}
+      onMouseEnter={e => setStyle({ display: 'block' })}
+      onMouseLeave={e => setStyle({ display: 'none' })}>
       <img src={image} alt="product photo" />
       <p className={s.title}>{title}</p>
-      <p className={s.descr}>{description}</p>
       <p className={s.price}>{price}$</p>
+      <div style={style}>
+        <button>Add to cart </button>
+      </div>
     </div>
   )
 }
